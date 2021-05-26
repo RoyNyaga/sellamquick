6ad718c0b6b5e2f41f8047b4d9d1f95b97209382
roynyaga <nyagaandreroy@gmail.com>
2021-05-26 18:47:44 +0100

    create authAction

---
+++ app/javascript/redux/actions/authActions.js
@@ -0,0 +1,11 @@
+const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS';
+
+
+const createUserSuccessAction = user => ({
+  type: CREATE_USER_SUCCESS,
+  user,
+});
+
+export {
+  createUserSuccessAction
+};
