0f0769ccfd3a445cc82f774db08a4196f28dba99
roynyaga <nyagaandreroy@gmail.com>
2021-05-28 02:00:10 +0100

    create interestSurveyPage

---
+++ app/javascript/containers/interest/interestSurveyPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class InterestSurveyPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <div>
+        <h1>hello this thing is working</h1>
+      </div>
+    );
+  }
+}
+ 
+export default InterestSurveyPage;
--- app/javascript/routes/routes.jsx
+++ app/javascript/routes/routes.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Test from "../components/Test";
+import InterestSurveyPage from "../containers/interest/interestSurveyPage";
 
 export default function Routing(){
   return(
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/test" exact component={Test} />
+        <Route path="/user-interest-survey" exact component={InterestSurveyPage} />
       </Switch>
     </Router>
   )
