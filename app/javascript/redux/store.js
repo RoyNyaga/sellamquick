6c74f68e155e1d1daae512e2c2ef344ca3e56224
roynyaga <nyagaandreroy@gmail.com>
2021-05-28 01:38:56 +0100

    setup redux dev tols

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,11 +1,24 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
+// Bellow is the general format for defining a redux store
+
+// const store = createStore(rootReducer, preloadedState, enhancer)
+
+// we use undefine when we have no preloadedState
+// createStore only accepts one enhancer as its third argument!
+// to use multiple enhancers we need to use redux compose 
+
+const composedEnhancer = compose(
+  applyMiddleware(thunk), 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 const store = createStore(
   rootReducer, 
-  applyMiddleware(thunk),
+  undefined,
+  composedEnhancer,
 );
 
 export default store;
