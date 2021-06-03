07c40958acd6088924d7719856d98ad46dadcf39
roynyaga <nyagaandreroy@gmail.com>
2021-06-03 02:48:50 +0100

    add code to category blueprint

--- app/blueprints/category_blueprint.rb
+++ app/blueprints/category_blueprint.rb
@@ -2,4 +2,7 @@ class CategoryBlueprint < Blueprinter::Base
   identifier :id
 
   fields :name, :slug, :description
+  field :photo do |category, options|
+    category.photo
+  end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  
   mount RailsAdmin::Engine => '/admin-dashboard', as: 'rails_admin'
   devise_for :admins
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
+
+  # categories 
+  get "/all-gategories", to: "categories#category_list"
+
+
+
   get '/*path' => 'pages#index'
+
 end
