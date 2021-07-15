94855eb3bcbfbc40360cd0b38a97f4c40796f422
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-15 22:35:49 +0100

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :interests
   has_many :interested_categories, through: :interests, source: "category"
+
+  validates :name, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :phone, presence: true, uniqueness: true
+  validates :location, presence: true
 end
