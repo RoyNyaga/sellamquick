e90ea1704b95538c7446e4673f2ab9ba26a51d4e
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-15 22:45:16 +0100

    setup friendly_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  #relationship definitions
   has_secure_password
   has_many :interests
   has_many :interested_categories, through: :interests, source: "category"
 
+  #validations
   validates :name, presence: true, uniqueness: true
   validates :password, presence: true
   validates :phone, presence: true, uniqueness: true
   validates :location, presence: true
+
+  #callbacks
+
 end
