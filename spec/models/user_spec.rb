80808bb264a17813213e0fd223846c322f53b766
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-16 22:27:36 +0100

    add first series of users test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  context "name validation" do 
+    context "when validating presence" do 
+      let(:user) { build(:user, name: "") }
+      it "users name must be present" do 
+        user.valid?
+        expect(user.errors.full_messages).to include("Name can't be blank")
+      end 
+    end 
+
+    context "when validating uniqueness" do 
+      let(:user1) { create(:user) }
+      it "users name must be unique" do 
+        user2 = build(:user, name: user1.name)
+        user2.valid?
+        expect(user2.errors.full_messages).to include("Name has already been taken")
+      end 
+    end 
+  end 
 end
